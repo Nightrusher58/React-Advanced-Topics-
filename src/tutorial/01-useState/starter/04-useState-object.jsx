09f39f60d8cee25582c704916345b93a9a366cc2
unknown <tobilobadurojaiye@gmail.com>
2023-05-29 17:25:38 -0400

    useEffect Intro

--- src/tutorial/01-useState/starter/04-useState-object.jsx
+++ src/tutorial/01-useState/starter/04-useState-object.jsx
@@ -1,24 +1,17 @@
 import { useState } from "react";
-let persons = [
-  { name: "John", age: "28", hobby: "Video Games" },
-  {
-    name: "Jack",
-    age: "25",
-    hobby: "Programming",
-  },
-];
 
 const UseStateObject = () => {
-  const [name, setName] = useState("John");
-  const [age, setAge] = useState("28");
-  const [hobby, setHobby] = useState("Video Games");
-
+  const [person, setPerson] = useState({
+    name: "John",
+    age: "28",
+    hobby: "Video Games",
+  });
   const handleClick = () => {
-    setName("Jack");
-    setAge("25");
-    setHobby("Programming");
+    setPerson((prevState) => {
+      return { ...prevState, name: "Jackie" };
+    });
   };
-
+  const { name, age, hobby } = person;
   return (
     <div>
       <h2>{name}</h2>
--- src/tutorial/02-useEffect/starter/01-code-example.jsx
+++ src/tutorial/02-useEffect/starter/01-code-example.jsx
@@ -1,12 +1,20 @@
-import { useState } from 'react';
+import { useState, useEffect } from "react";
 
 const CodeExample = () => {
   const [value, setValue] = useState(0);
+  const test = () => {
+    console.log("Hello There its test");
+  };
 
+  test();
+
+  useEffect(() => {
+    console.log("Hey its useEffect, and i will only console log once");
+  }, []);
   return (
     <div>
       <h1>value : {value}</h1>
-      <button className='btn' onClick={() => setValue(value + 1)}>
+      <button className="btn" onClick={() => setValue(value + 1)}>
         click me
       </button>
     </div>
