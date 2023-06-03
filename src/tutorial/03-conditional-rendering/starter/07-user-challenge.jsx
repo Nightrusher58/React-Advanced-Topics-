ed3174fed5d1a92d009b16af20c29d63f91d6a0c
unknown <tobilobadurojaiye@gmail.com>
2023-06-03 00:36:07 -0400

    conditional rendering components, passed props 'functions' and 'values' through components and rendered

--- src/tutorial/03-conditional-rendering/starter/07-user-challenge.jsx
+++ src/tutorial/03-conditional-rendering/starter/07-user-challenge.jsx
@@ -1,5 +1,48 @@
+import { useState } from "react";
+
 const UserChallenge = () => {
-  return <h2>user challenge</h2>;
+  const [user, setUser] = useState(null);
+
+  const login = () => {
+    setUser({ name: "Hello Wale" });
+  };
+
+  const logout = () => {
+    setUser(null);
+  };
+
+  return (
+    <div>
+      {" "}
+      {user ? (
+        <WelcomePage Uname={user} lg={logout} />
+      ) : (
+        <LogoutPage li={login} />
+      )}
+    </div>
+  );
+};
+
+const WelcomePage = (props) => {
+  return (
+    <div>
+      <h1>Hello there {props.Uname.name} </h1>
+      <button type="button" onClick={props.lg}>
+        log Out
+      </button>
+    </div>
+  );
+};
+
+const LogoutPage = (props) => {
+  return (
+    <div>
+      <h1>Enter your credentials</h1>
+      <button type="button" onClick={props.li}>
+        login
+      </button>
+    </div>
+  );
 };
 
 export default UserChallenge;
