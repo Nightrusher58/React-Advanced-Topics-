090015a99dfbe6418292bc3349a7757c807c0f95
unknown <tobilobadurojaiye@gmail.com>
2023-06-02 23:31:23 -0400

    conditional rendering of components, elements etc with tenary operators

--- src/tutorial/03-conditional-rendering/starter/06-toggle-challenge.jsx
+++ src/tutorial/03-conditional-rendering/starter/06-toggle-challenge.jsx
@@ -1,5 +1,28 @@
+import { useState } from "react";
+
 const ToggleChallenge = () => {
-  return <h2>toggle challenge</h2>;
+  const [isTrue, setTrue] = useState(false);
+
+  const handleClick = () => {
+    setTrue(!isTrue);
+  };
+
+  return (
+    <div>
+      <button onClick={handleClick} type="button">
+        toggle
+      </button>
+      {isTrue ? <SomeComponent /> : null}
+    </div>
+  );
+};
+
+const SomeComponent = () => {
+  return (
+    <div>
+      <h1>SomeText</h1>
+    </div>
+  );
 };
 
 export default ToggleChallenge;
