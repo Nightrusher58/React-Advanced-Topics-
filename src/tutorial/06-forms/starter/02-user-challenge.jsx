90ef5e3d35a03f1c27b9f73dafdd84331ae9675a
unknown <tobilobadurojaiye@gmail.com>
2023-06-07 21:48:10 -0400

    react form, dlete,update,input

--- src/tutorial/06-forms/starter/02-user-challenge.jsx
+++ src/tutorial/06-forms/starter/02-user-challenge.jsx
@@ -1,20 +1,72 @@
+import { data } from "../../../data";
+import { useState } from "react";
+
 const UserChallenge = () => {
+  const [display, setDisplay] = useState(data);
+  const [name, setName] = useState("");
+  const [id, setId] = useState(5);
+  const handleChange = (e) => {
+    setName(e.target.value);
+  };
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    setId((prevId) => {
+      return prevId + 1;
+    });
+
+    setDisplay((prevState) => {
+      const newState = [...prevState, { id: id, name: name }];
+      return newState;
+    });
+
+    setName("");
+  };
+
+  const handleDelete = (id) => {
+    const setNewFolks = display.filter((person) => {
+      return person.id !== id;
+    });
+    setDisplay(setNewFolks);
+  };
   return (
     <div>
-      <form className='form'>
+      <form className="form">
         <h4>Add User</h4>
-        <div className='form-row'>
-          <label htmlFor='name' className='form-label'>
+        <div className="form-row">
+          <label htmlFor="name" className="form-label">
             name
           </label>
-          <input type='text' className='form-input' id='name' />
+          <input
+            type="text"
+            className="form-input"
+            id="name"
+            value={name}
+            onChange={handleChange}
+          />
         </div>
 
-        <button type='submit' className='btn btn-block'>
+        <button type="submit" className="btn btn-block" onClick={handleClick}>
           submit
         </button>
       </form>
-      {/* render users below */}
+      <div>
+        {display.map((person) => {
+          const { name, id } = person;
+          return (
+            <div key={id}>
+              <h2>{name}</h2>
+              <button
+                type="button"
+                className="btn"
+                onClick={() => handleDelete(id)}
+              >
+                Remove
+              </button>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
